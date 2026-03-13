3fbde3cb83e47af0e6e9c57fc815aad6d13ffa09
N T Hari Haran <nt.hariharan.academic@gmail.com>
2026-03-13 10:06:49 +0530

    Replaced request newcontext with just request fixture

--- 12-03-2026/tests/loginWithToken.spec.js
+++ 12-03-2026/tests/loginWithToken.spec.js
@@ -1,23 +1,20 @@
-const { test, expect } = require('@playwright/test');
-
-test.beforeAll(async () => {
-    const api = await request.newContext();
+const { test, expect  } = require('@playwright/test');
+let token;
+test.beforeAll(async ({request}) => {
     const tokenEndPoint = "https://rahulshettyacademy.com/api/ecom/auth/login";
-    
     const login_payload = { userEmail: process.env.MAILID, userPassword: process.env.PWD };
-    const orderPayload = { orders: [{ country: "Anguilla", productOrderedId: "6960ea76c941646b7a8b3dd5" }] };
 
-    const apiResp = await api.post(tokenEndPoint, {
+    const apiResp = await request.post(tokenEndPoint, {
         data: login_payload
     });
 
     expect(apiResp.ok()).toBeTruthy();
     const json_apiResp = await apiResp.json();
-    const token = json_apiResp.token;
+    token = json_apiResp.token;
     expect(json_apiResp.message).toMatch(/Login Successfull/i);
 });
 
 
 test("Direct Login with Token @DIRECT", async ({ browser }) => {
-
+    console.log("Token =>", token);
 });
