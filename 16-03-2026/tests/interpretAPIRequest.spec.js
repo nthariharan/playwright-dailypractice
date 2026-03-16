76c8cdeb1907f517346864441486195bb8dd590f
N T Hari Haran <nt.hariharan.academic@gmail.com>
2026-03-16 16:03:41 +0530

    added +1 practice file

--- 16-03-2026/tests/interpretAPIRequest.spec.js
+++ 16-03-2026/tests/interpretAPIRequest.spec.js
@@ -1,8 +1,33 @@
 const {customTest} = require('../fixtures/authenticatedPage');
+const {expect} = require('@playwright/test');
 
-customTest('Interpret API Request @INTER', async({token,orderId})=>{
-    console.log("**Token ==>", token);
-    console.log("**Order ID =>", orderId);
 
+customTest('Interpret API Request @INTER', 
+    /*** @param {{authPage : import('@playwright/test').Page , orderId : string}} */
+    async({orderId,authPage})=>{
+    const navbar = authPage.getByRole('navigation');
+    const btnOrders = navbar.getByRole('button',{name:/orders/i});
+    const orderTable = authPage.getByRole('row');
+    const orderRow = orderTable.filter({hasText : orderId});
+    const orderView = orderRow.getByRole('button',{name:/view/i});
+    const getOrderEndPoint = "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*"  
+    const error = authPage.getByText('You are not authorize to view this order');
+
+    /***Listener */
+    authPage.route(getOrderEndPoint, async (route,request)=>{
+        let origURL = request.url();
+        let mockURL = origURL.replace(/.{3}$/,"123");
+        route.continue({url : mockURL});
+    });
+
+
+    await authPage.goto("https://rahulshettyacademy.com/client");
+    await expect(authPage).toHaveURL(/\/dashboard\/dash$/);
+    await expect(navbar).toBeVisible();
+    await btnOrders.click();
+    await expect(authPage).toHaveURL(/\/dashboard\/myorders$/);
+    await expect(orderTable.last()).toBeVisible();
+    await orderView.click();
+    await expect(error).toBeVisible(); 
 });
 
